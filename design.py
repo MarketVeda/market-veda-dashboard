6fdcb857cf922ea6963d7bab59078ed2af61a9c5
MarketVeda <marketvedaresearch@gmail.com>
2026-06-26 12:06:53 +0530

    Fix all fstring

--- design.py
+++ design.py
@@ -156,8 +156,7 @@ def generate(sym, data, tech, fp, sc, sent, pos, neg, risk_r):
             cells+=f"<td{st}>{v:,.0f}{suf}</td>" if isinstance(v,(int,float)) else f"<td{st}>N/A</td>"
         return f"<tr><td>{lbl}</td>{cells}</tr>"
 
-    _last_style=" style='background:#0A2A1E;'"
-    qtr_beat="".join(f'<td class="beat"{"" if i<len(qtrs)-1 else _last_style}>BEAT</td>' for i in range(len(qtrs)))
+    qtr_beat="".join(f'<td class="beat"{"" if i<len(qtrs)-1 else " style=\"background:#0A2A1E;\""}>BEAT</td>' for i in range(len(qtrs)))
     qtr_body=qrow("Revenue","rev")+qrow("Net Profit","np")+qrow("EPS (₹)","eps")+qrow("OPM %","opm","%")+f"<tr><td>Result</td>{qtr_beat}</tr>"
 
     # Returns table
@@ -224,11 +223,11 @@ def generate(sym, data, tech, fp, sc, sent, pos, neg, risk_r):
     neg_html="".join(f'<div class="trg">{t}</div>' for t in neg[:4])
 
     # OI rows
-    _oi_parts=[]
-    for r in (fno_oi or [])[-5:]:
-        _rd=r["date"][5:]; _roi=fvol(r["oi"]); _rltp=r["ltp"]
-        _oi_parts.append(f"<tr><td>{_rd}</td><td style='text-align:right;'>{_roi}</td><td style='text-align:right;'>₹{_rltp:,.1f}</td></tr>")
-    oi_rows="".join(_oi_parts) or '<tr><td colspan="3" style="color:#555;">Non-FnO stock</td></tr>' 
+    oi_rows="".join(
+        f'<tr><td>{r["date"][5:]}</td><td style="text-align:right;">{fvol(r["oi"])}</td>'
+        f'<td style="text-align:right;">₹{r["ltp"]:,.1f}</td></tr>'
+        for r in (fno_oi or [])[-5:]
+    ) or '<tr><td colspan="3" style="color:#555;">Non-FnO stock</td></tr>'
 
     # Rec tag
     act=sc["action"]; ac=sc["ac"]
